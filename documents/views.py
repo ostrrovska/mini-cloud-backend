46a2b75e43d64618d830aa6c2ef8426a005a3b76
ostrrovska <ostrovskakatia@gmail.com>
2025-09-13 09:53:42 +0300

    Add authentication

--- documents/views.py
+++ documents/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework import viewsets
 from .models import Document
 from .serializers import DocumentSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class DocumentViewSet(viewsets.ModelViewSet):
-    queryset = Document.objects.all()
     serializer_class = DocumentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Цей view має повертати список документів
+        тільки для поточного аутентифікованого користувача.
+        """
+        return Document.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """
+        При збереженні нового документа, автоматично
+        прив'язуємо його до поточного користувача.
+        """
+        serializer.save(user=self.request.user)
